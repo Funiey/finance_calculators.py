410935b97b7a59bc86845e328df4da6ff8dbc344
F Maitakhole <127227319+Funiey@users.noreply.github.com>
2024-07-09 08:56:37 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 # a program that allows the user to access two different financial calculators:
 #  an investment calculator and a home loan repayment calculator.
-
+# funani
 import math
 
 def output():
